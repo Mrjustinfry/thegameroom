8ba745cae91a79dfd44d300f98cb79e32cd24fd5
Justin Fry <mrjustinfry@gmail.com>
2018-10-04 14:16:08 -0400

    added markup templates

---
+++ public/hub.js
@@ -0,0 +1,32 @@
+﻿'use strict'
+
+function signupTemplate() {
+    return `<div>
+            <form for="signup" id="signup">
+                <fieldset>
+                    <h4 class="signUpHead">Sign Up!</h4>
+                    <label>First Name: <input type="text" min="3" class="firstName" required /> </label>
+                    <label>Last Name: <input type="text" min="3" class="lastName" required /></label>
+                    <label>Choose a Username: <input type="text" min="3" maxlength="15" class="userName" required /></label>
+                    <div id="#"></div>
+                    <label>Password: <input type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required /></label>
+                    <label>Re-enter Password: <input type="password" min="5" maxlength="20" required /></label>
+                    <div id="#"></div>
+                    <label>Switch friend code: <input type="text" class="switch" placeholder="SW-1234-5678-9000" pattern="[SW][ -][0-9]{4}[ -][0-9]{4}[ -][0-9]{4}" /></label>
+                    <label>PS4 Gamertag: <input type="text" class="ps4" /></label>
+                    <label>Xbox Gamertag: <input type="text" class="xbox" /></label>
+                    <label>
+                        Preferred Platform:
+                        <select>
+                            <option value="Nintendo">Nintendo</option>
+                            <option value="Playstation">Playstation</option>
+                            <option value="Xbox">Xbox</option>
+                            <option value="PC">PC</option>
+                        </select>
+                    </label>
+                    <button>Submit</button>
+                </fieldset>
+            </form>
+        </div>`
+};
+
--- server.js
+++ server.js
@@ -1,16 +1,122 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+mongoose.Promise = global.Promise;
 
 app.use(express.static('public'));
 
 
+const { Posts, Users } = require('./models');
+const { DATABASE_URL, PORT } = require('./config');
+
+
+//GET request for hub
+app.get('/hub', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users.map(user => {
+                return {
+                    userName: user.userName,
+                    switch: user.switch,
+                    playstation: user.playstation,
+                    xbox: user.xbox,
+                    platform: user.platform
+                }
+            }))
+                .catch(err => {
+                    console.log(err);
+                    res.status({ message: "Internal Server Error" })
+                })
+        })
+});
+
+//POST request for hub
+app.post('/signup', (req, res) => {
+    const requiredInfo = ['firstName', 'lastName', 'userName', 'passWord'];
+    requiredInfo.forEach(info => {
+        if (!(info in req.body)) {
+            const msg = `Missing ${info}`
+            console.log(msg);
+            res.send(msg)
+        }
+    });
+
+    Users.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userName: req.body.userName,
+        passWord: req.body.passWord,
+        switch: req.body.switch,
+        playstation: req.body.playstation,
+        xbox: req.body.xbox,
+        platform: req.body.platform
+    })
+
+        .then(user => {
+            res.status(201).json({
+                id: user._id,
+                userName: user.userName,
+                switch: user.switch,
+                playstation: user.playstation,
+                xbox: user.xbox,
+                platform: user.platform
+            })
+        })
+
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: 'something went wrong' });
+        });
+});
+
+
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(
+            databaseUrl,
+            err => {
+                if (err) {
+                    return reject(err);
+                }
+                server = app
+                    .listen(port, () => {
+                        console.log(`Your app is listening on port ${port}`);
+                        resolve();
+                    })
+                    .on("error", err => {
+                        mongoose.disconnect();
+                        reject(err);
+                    });
+            }
+        );
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing Server');
+            server.close(err => {
+                if (err) {
+                    {
+                        return reject(err);
+                    }
+                    resolve();
+                }
+            })
+        })
+    })
+}
+
+
 
 if (require.main === module) {
-    app.listen(process.env.PORT || 8080, function () {
-        console.info(`App listening on ${this.address().port}`);
-    });
+    runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = app;
